30612e8c04c82d5671047ab637df91b4e2906fa5
Vencel Koczka <koczka990@gmail.com>
2026-05-23 22:47:41 +0200

    feat: update MaterialApp theme with Modulor color scheme and Archivo Narrow font

--- modulor_app/lib/main.dart
+++ modulor_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_theme.dart';
 import 'models/puzzle.dart';
 import 'widgets/game_screen.dart';
 
@@ -15,7 +18,12 @@ class ModulorApp extends StatelessWidget {
       title: 'Modulor',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          surface: AppColors.background,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+        textTheme: GoogleFonts.archivoNarrowTextTheme(),
         useMaterial3: true,
       ),
       home: const GameScreen(puzzle: kHardcodedPuzzle),
