5ce59f8b14c689394a7d8da05ec7c0c25e487c57
Vencel Koczka <koczka990@gmail.com>
2026-05-24 15:50:53 +0200

    feat: implement WelcomeScreen with scale animation

--- modulor_app/lib/screens/welcome_screen.dart
+++ modulor_app/lib/screens/welcome_screen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../app_theme.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
 
   @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _scale;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+    _scale = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+    _controller.forward().then((_) {
+      Future.delayed(const Duration(milliseconds: 400), () {
+        if (mounted) context.go('/sets');
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Welcome')));
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: ScaleTransition(
+          scale: _scale,
+          child: Container(
+            width: 80,
+            height: 80,
+            color: AppColors.primary,
+          ),
+        ),
+      ),
+    );
   }
 }
